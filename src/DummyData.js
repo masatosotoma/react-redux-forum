6c9e7d8833465f2dc48af65bf943d7f7a1281299
MasatoSotoma <masatosotoma@gmail.com>
2024-11-19 22:15:36 -0500

    Created dummydata

---
+++ src/DummyData.js
@@ -0,0 +1,22 @@
+export const PostsData = [
+  {
+    id: 1,
+    name: "ShinCode",
+    content: "Redux Study Now",
+  },
+  {
+    id: 2,
+    name: "noname",
+    content: "ShinCode Easy to understand",
+  },
+  {
+    id: 3,
+    name: "Engineer begginer",
+    content: "Udemy take the course",
+  },
+  {
+    id: 4,
+    name: "Stack Overflow",
+    content: "Engineer's stabucks",
+  },
+];
