36d2f2d3969b5a53da296a1b6c6c436dd706808c
MasatoSotoma <masatosotoma@gmail.com>
2024-11-24 21:08:58 -0500

    Added the basic structure for the forum

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import "./App.css";
 import { useSelector } from "react-redux";
 
 function App() {
-  return <div className="App"></div>;
+  const postList = useSelector((state) => state.posts.value);
+  console.log(postList);
+  return (
+    <div className="App">
+      <div>
+        <h1>React-Redux-Forum</h1>
+      </div>
+      <div>
+        <div className="addPost">
+          <input type="text" placeholder="Name"></input>
+          <input type="text" placeholder="Post Contents"></input>
+          <button>Post</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
